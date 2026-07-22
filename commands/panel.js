9179c7d8b698db1017ad9316609fe3e0157456ce
modzelevich-cloud <modzelevich@gmail.com>
2026-07-22 19:32:30 +0200

    Update panel.js

--- commands/panel.js
+++ commands/panel.js
@@ -1,1 +1,65 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
+} = require("discord.js");
 
+
+module.exports = {
+
+data: new SlashCommandBuilder()
+.setName("panel")
+.setDescription("Creates the BNR application panel"),
+
+
+async execute(interaction){
+
+
+const embed = new EmbedBuilder()
+
+.setTitle("⚔️ BNR Clan Applications")
+
+.setDescription(
+`
+Welcome to BNR recruitment.
+
+Click the button below to start your application.
+
+You will be asked for:
+• Roblox Rivals Level
+• Roblox Rivals Rank
+
+After submitting, a private application channel will be created.
+`
+)
+
+.setColor("Red");
+
+
+const button = new ButtonBuilder()
+
+.setCustomId("apply")
+
+.setLabel("📝 Apply")
+
+.setStyle(ButtonStyle.Success);
+
+
+
+await interaction.reply({
+
+embeds:[embed],
+
+components:[
+new ActionRowBuilder()
+.addComponents(button)
+]
+
+});
+
+
+}
+
+};
