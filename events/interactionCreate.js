452fc44ac4a2f1dfb159917c7f146568299c4a45
modzelevich-cloud <modzelevich@gmail.com>
2026-07-22 19:32:51 +0200

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,38 @@
+const {
+ChannelType,
+PermissionFlagsBits,
+EmbedBuilder
+} = require("discord.js");
 
+
+module.exports = (client)=>{
+
+
+client.on("interactionCreate", async interaction=>{
+
+
+if(interaction.isButton()){
+
+
+if(interaction.customId==="apply"){
+
+
+await interaction.reply({
+
+content:"What is your Roblox Rivals Level?",
+
+ephemeral:true
+
+});
+
+
+}
+
+}
+
+
+
+});
+
+
+};
